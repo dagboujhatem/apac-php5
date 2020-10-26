2155c8ce6ba471d9bad7e6dc09282a01fa834994
dagboujhatem <dagboujhatem@gmail.com>
2020-10-26 09:58:26 +0100

    Ajouter le premier TP

---
+++ tp1.php
@@ -0,0 +1,24 @@
+<?php 
+// la variable titre stocke le titre de la page
+$titre = "Welcome PHP"
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		<?php 
+			echo $titre
+		?>
+	</title>
+</head>
+<body>
+
+	<?php
+		# affichage de contenu de la page 
+		echo("Hello Again !!")
+
+	?>
+
+</body>
+</html>
