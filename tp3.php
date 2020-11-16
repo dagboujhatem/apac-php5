c89b30e5630d2a04b7e2636e47bf5a078adfab00
dagboujhatem <dagboujhatem@gmail.com>
2020-11-16 10:23:20 +0100

    ajouter un tp3

---
+++ tp3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TP3 en PHP</title>
+</head>
+<body>
+	<?php
+		// Définition d'un variable name de 3 manières:
+		// $name;
+		// $name = null;
+		$name = "Ahmed";
+
+		// Tester si le variable contient un valeur
+		if(isset($name))
+		{
+			// Le name est définie
+			echo("<h3>le name est définie: $name </h3>");
+		}else{
+			// Le name n'est pas définie
+			echo("<h3>le name n'est pas définie: $name </h3>");
+		}
+
+	?>
+</body>
+</html>
