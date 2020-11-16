f8f357533569eb2bd73eaa9712e5668acb4d216d
dagboujhatem <dagboujhatem@gmail.com>
2020-11-16 10:57:31 +0100

    add sign up

---
+++ signup.php
@@ -0,0 +1,36 @@
+<?php
+
+// first step: récupération des valeurs à partir de HTML et en se basant sur les "names" de chaque input
+$first_name = $_POST["firstname"];
+$last_name = $_POST["lastname"];
+$email = $_POST["email"];
+$password = $_POST["password"];
+$password_confirmation = $_POST["passwordconfirmation"];
+
+echo ("<h1> Welcome $first_name $last_name $email $password </h1>");
+
+// second step: vérifications des champs 
+if (isset($first_name)) {
+	// Affichage d'erreur de first name 
+	echo "first name is null <br>";
+}
+if (isset($last_name)) {
+	// Affichage d'erreur de last name 
+	echo "last name is null <br>";
+}
+if (isset($email)) {
+	// Affichage d'erreur d' email
+	echo "email is null <br>";
+}
+if (isset($password)) {
+	// Affichage d'erreur de password 
+	echo "password is null <br>";
+}
+if (isset($password_confirmation)) {
+	// Affichage d'erreur de password confirmation 
+	echo "password confirmation is null <br>";
+}
+
+// step 3 : verification password and password confirmation is equal
+
+?>
---
+++ tp4.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>TP4 en PHP</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<form class="offset-3 col-6 mt-5" method="post" action="signup.php">
+				<div class="form-group">
+					<label for="firstname">First name</label>
+					<input type="text" name="firstname" id="firstname" placeholder="Type your first name here" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="lastname">Last Name</label>
+					<input type="text" name="lastname" id="lastname" placeholder="Type your last name here" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="email">E-mail</label>
+					<input type="email" name="email" id="email" placeholder="Type your e-mail here" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" name="password" id="password" placeholder="Type your password here" class="form-control">
+				</div>
+				<div class="form-group">
+					<label for="passwordconfirmation">Password</label>
+					<input type="password" name="passwordconfirmation" id="passwordconfirmation" placeholder="Type your password confirmation here" class="form-control">
+				</div>
+				<div>
+					<input type="submit" value="Sign Up" class="btn btn-success btn-block">
+				</div>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
